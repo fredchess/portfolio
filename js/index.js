d0fb8232defbe306ee4f8ec191ecfc5b9cf25e78
fredchess97 <fredericktabi30@gmail.com>
2022-10-07 08:29:16 +0100

    wip

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$('document').ready(function () {
+    $('.nav-item').click(function (e) {
+        $('.nav-item.active').removeClass('active')
+        $(this).addClass('active')
+    })
+
+    $('#collapsebtn').click(e => {
+        let leftbar = $('#left-bar')
+        let main = $('.main')
+
+        main.addClass('blur')
+        leftbar.addClass('left-0')
+        leftbar.removeClass('left-[-350px]')
+
+        $(document).mousedown(function (e) {
+            if ((e.target.id !== 'left-bar' && $(e.target).closest('#left-bar').length === 0) || e.target.id === 'close') {
+                main.removeClass('blur')
+                leftbar.removeClass('left-0')
+                leftbar.addClass('left-[-350px]')
+            }
+        });
+    })
+})
---
+++ js/tailwind-config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                black: '#2B2B2B',
+                gray: '#767676',
+                'light-gray': '#F0F0F6',
+                orange: '#FFB400',
+                green: '#7EB942'
+            }
+        }
+    }
+}

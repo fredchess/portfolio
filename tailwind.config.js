397a4ccbb2f84b3f53e3c3493e3f3d2f925abd21
fredchess97 <fredericktabi30@gmail.com>
2022-10-08 04:24:35 +0100

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        black: '#2B2B2B',
+        gray: '#767676',
+        'light-gray': '#F0F0F6',
+        orange: '#FFB400',
+        green: '#7EB942'
+      }
+    }
+  },
+  plugins: [],
+}
